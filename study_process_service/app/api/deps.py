22f55e84fbb675d23a0f7c35cfb22073ea383522
Superpuperbober <ivanborshevsky2002mgdn@gmail.com>
2026-04-17 13:59:29 +1000

    UPD: рефакторинг + удаление связи возможен путь

--- study_process_service/app/api/deps.py
+++
@@ -1,9 +0,0 @@
-from typing import Annotated
-
-from fastapi import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from app.core import get_async_session
-
-
-DbSession = Annotated[AsyncSession, Depends(get_async_session)]
