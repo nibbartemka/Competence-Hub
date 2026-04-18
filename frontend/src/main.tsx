083a3da9becd82dbda559b439736f1dbf7e0ade9
Artem Shuliatev <slenderartem2003@gmail.com>
2026-04-18 22:29:38 +1000

    FEAT(frontend): Добавлен роутер

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,26 @@
-import ReactDOM from "react-dom/client";
+// import ReactDOM from "react-dom/client";
 
-import App from "./App";
-import "./styles.css";
+// import App from "./App";
+// import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+// ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import KnowledgeGraph from './KnowledgeGraph'; // новый компонент
+
+const router = createBrowserRouter([
+    { path: "/disciplines/:disciplineId/knowledge", element: <KnowledgeGraph /> }, // новый маршрут
+]);
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+
+root.render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
