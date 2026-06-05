25cdae49d4729bd010b5e029641059a4b60a9385
Artem Shuliatev <slenderartem2003@gmail.com>
2026-06-05 11:45:38 +1000

    FEAT(schemas): Добавлена ссхема для редактирования элемента ЗУВ

--- backend/app/schemas/knowledge_elements.py
+++ backend/app/schemas/knowledge_elements.py
@@ -33,8 +33,25 @@ class StructuredMasterKnowledgeElementCreate(BaseModel):
     discipline_id: UUID
     topic_id: UUID
     subject_area_description: str = Field(min_length=1)
-    automated_skill_element_id: UUID
+    automated_skill_element_ids: list[UUID] = Field(min_length=1)
     domain_objects: list[MasterElementDomainObjectCreate] = Field(min_length=1)
+
+
+class StructuredMasterKnowledgeElementUpdate(BaseModel):
+    name: str = Field(min_length=1, max_length=255)
+    description: str | None = None
+    topic_id: UUID
+    subject_area_description: str = Field(min_length=1)
+    automated_skill_element_ids: list[UUID] = Field(min_length=1)
+    domain_objects: list[MasterElementDomainObjectCreate] = Field(min_length=1)
+
+
+class StructuredSkillKnowledgeElementUpdate(BaseModel):
+    name: str = Field(min_length=1, max_length=255)
+    description: str | None = None
+    topic_id: UUID
+    operation_ref: str = Field(min_length=1, max_length=255)
+    realized_knowledge_element_ids: list[UUID] = Field(min_length=1)
 
 
 class KnowledgeElementRead(BaseModel):
